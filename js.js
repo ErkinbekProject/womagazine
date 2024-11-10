d0e701469217095ecf46b60e71b2fd28c07f8887
ErkinbekProject <erkinbekanarkulov123@gmail.com>
2024-11-10 17:54:59 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,13 @@
+let login = document.querySelector('.login')
+
+
+let toggleLogin = ()=>{
+    login.classList.toggle('active')
+}
+
+
+let thanks = document.querySelector('.thanks')
+
+let toggleThanks = ()=>{
+    thanks.classList.toggle('active')
+}
